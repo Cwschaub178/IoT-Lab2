a13555bcd19252a90fface74f076bc7811d8bb31
Chris Schaub <92747758+Cwschaub178@users.noreply.github.com>
2024-10-13 17:54:28 -0500

    Update wifi_client.py

--- wifi_client.py
+++ wifi_client.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = "192.168.1.209" # IP address of your Raspberry PI
+HOST = "192.168.123.132" # IP address of your Raspberry PI
 PORT = 65432          # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
